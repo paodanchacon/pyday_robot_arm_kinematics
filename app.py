bca020f43fc4c22b15fcbe9c2a0640c603f1eebe
Daniela <pao.dan.cha.con@gmail.com>
2024-11-12 00:18:50 -0400

    add hack to avoid complex numbers

--- app.py
+++ app.py
@@ -7,35 +7,37 @@ from utils import draw_robot, draw_grid
 from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
 
-# Initialize Pygame
+# Iniciar Pygame
 pygame.init()
 
-# Set up display
+# Configurar display
 width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Basic Pygame Application")
+pygame.display.set_caption("Kinematics of a Robot Arm")
+fac_size = 100
 
-# Define colors
+# Definir colores
 WHITE = (255, 255, 255)
 
-
+# Dibujar sliders
 q0_slider = Slider(screen, 50, height-100, 200, 20, min=0, max=2*math.pi, step=0.01)
 q1_slider = Slider(screen, 50, height-50, 200, 20, min=0, max=2*math.pi, step=0.01)
 
+# Texto de los angulos
 q0_text = TextBox(screen, 270, height-100, 50, 30)
 q1_text = TextBox(screen, 270, height-50, 50, 30)
 
+# Texto de la posicion del click
 x_text = TextBox(screen, width-80, height-100, 50, 30)
 y_text = TextBox(screen, width-80, height-50, 50, 30)
 
-
-fac_size = 100
-#parametros del robot
-a0, a1 = 2, 2
+# Parametros del robot
+a0, a1 = 1, 1
 x, y = None, None
 q0, q1 = 0, 0
 is_computed = False
 x_t, y_t = 0, 0
+
 # Main loop
 running = True
 while running:
@@ -45,7 +47,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Get the position of the click
+            # Obtener la posicion del click
             x, y = pygame.mouse.get_pos()
             x_t, y_t = x, y
             x = (x - width // 2) / fac_size
@@ -67,7 +69,8 @@ while running:
             print("posicion invalida amigo!!!!☠️☠️☠️☠️☠️")
             phi = math.atan2(y, x)
             print(phi)
-            l = a0 + a1
+            # hack para evitar numeros complejos
+            l = a0 + a1 - (a0 + a1) * 0.01
             x_hat = l * math.cos(phi)
             y_hat = l * math.sin(phi)
             print(x_hat, y_hat)
@@ -82,7 +85,7 @@ while running:
     # Fill the screen with white
     screen.fill(WHITE)
     draw_grid(screen, 50)
-    pygame.draw.circle(screen, (255,0,0), (x_t,y_t), 5, 2)
+    pygame.draw.circle(screen, (255,0,0), (x_t,y_t), 9, 3)
     draw_robot(screen, q0, q1, a0, a1, fac_size)
 
     # Draw the sliders and text boxes
